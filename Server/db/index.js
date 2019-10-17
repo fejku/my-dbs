5165b6bee84d22e7f5dd6234cba5783f0604728a
Krystian Kalita <krystian.kalita@zeto.olsztyn.pl>
2019-10-17 14:30:17 +0200

    rozwiązanie aktualizujące prawidłowo bazę z pumą, TODO: sprawdzić czemu nie działa return connect z dajKlient

--- Server/db/index.js
+++ Server/db/index.js
@@ -1,8 +1,15 @@
-const { Pool } = require('pg')
+const { Pool, Client } = require('pg')
 const config = require('./config');
 
 const pool = new Pool(config);
 
+const dajKlienta = (nazwaBazy) => {
+  configBazy = {...config};
+  configBazy.database = nazwaBazy;
+  return new Client(configBazy);
+}
+
 module.exports = {
   query: (text, params) => pool.query(text, params),
+  dajKlienta,
 }
--- Server/my-dbs/zapis-formularza.js
+++ Server/my-dbs/zapis-formularza.js
@@ -3,13 +3,27 @@ const db = require('../db');
 const zapisFormularza = async (req, res) => {
   const { baza, wersjaPumy, schematy } = req.body;
 
-  await db.query(`update ostatnio_uzyte set baza = $1, fk_wepu = $2;`, 
-    [baza, wersjaPumy]);
-
-  
-  for (const schemat of schematy) {
-    await db.query(`update wersja_schematu set wersja_schematu = $1 where id = $2`, 
-      [schemat.wersja_schematu, schemat.id]);
+  let client;
+  try {
+    await db.query(`update ostatnio_uzyte set baza = $1, fk_wepu = $2;`, 
+      [baza, wersjaPumy]);
+    
+    client = db.dajKlienta(baza);
+    await client.connect();
+    await client.query(`begin`);
+    for (const schemat of schematy) {
+      await db.query(`update wersja_schematu set wersja_schematu = $1 where id = $2`, 
+        [schemat.wersja_schematu, schemat.id]);
+          
+      await client.query(`update admi.moduly set wersja_schematu = $1 where nazwa_schematu = $2`, 
+        [schemat.wersja_schematu, schemat.nazwa_schematu]);
+    }
+    await client.query('commit');
+  } catch(err) {
+    console.log(err);
+    await client.query('rollback');
+  } finally {
+    client.end();
   }
 
   res.sendStatus(200);
